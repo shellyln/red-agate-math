be677c7e133fec7cb33c4e12869a41a1252a14a0
shellyln <shellyln@users.noreply.github.com>
2017-12-05 21:47:55 +0900

    v0.0.2

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Copyright (c) 2017, Shellyl_N and Authors
+// license: ISC
+// https://github.com/shellyln
+
+
+export * from './math/ArrayPolynomialRing';
+export * from './math/Equation';
+export * from './math/Field';
+export * from './math/Gf2Ext';
+export * from './math/Gf2PrimeHelper';
+export * from './math/GfPrime';
+export * from './math/RealField';
