a1cb6dc7d04eff9a0e3d851edcb5475e7aab9e42
shellyln <shellyln@users.noreply.github.com>
2017-12-10 08:48:48 +0900

    Add error correction classes.

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,7 @@ export * from './math/Gf2Ext';
 export * from './math/Gf2PrimeHelper';
 export * from './math/GfPrime';
 export * from './math/RealField';
+
+export * from './error-correction/BCH';
+export * from './error-correction/CRC32';
+export * from './error-correction/ReedSolomon';
